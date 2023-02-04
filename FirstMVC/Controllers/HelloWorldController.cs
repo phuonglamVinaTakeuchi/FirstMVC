dd68947eaf35baca7fb33e08c7bb182d78165903
Lam Nguyen <lam.nguyen@eastgate-software.com>
2023-02-04 14:42:05 +0700

    Begin for create Dog Controller

---
+++ FirstMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVC.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		private static readonly List<DogViewModel> _dogs = new();
+
+		public HelloWorldController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_dogs);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			var dog = new DogViewModel();
+			return View(dog);
+		}
+
+		[HttpPost]
+		public IActionResult Create(DogViewModel dogViewModel)
+		{
+			_dogs.Add(dogViewModel);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
